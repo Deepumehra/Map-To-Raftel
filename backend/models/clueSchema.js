84fa263d337d569dd6c90eee74cc476aa82ba604
Deepumehra <deepumehra1729@gmail.com>
2024-10-30 20:09:32 +0530

    User schema is added

--- backend/models/clueSchema.js
+++ backend/models/clueSchema.js
@@ -2,7 +2,10 @@ const mongoose=require('mongoose');
 const ClueSchema=new mongoose.Schema({
     clueId:String,
     description:String,
-    hint:String,
+    hint:{
+        type:String,
+        select:false
+    },
     location:String,
     sequenceOrder:Number
 });
---
+++ backend/models/userSchema.js
@@ -0,0 +1,40 @@
+const mongoose=require('mongoose');
+const UserSchema=new mongooose.Schema({
+    userId:String,
+    userName:{
+        type:String,
+        required:[true,'User Name is required'],
+        minlength:[5,'User Name  must be atleast 5 characters'],
+        trim:true
+    },
+    fullName: {
+      type: String,
+      required: [true, 'Full name is required'],
+      minlength: [5, 'Full name must be at least 5 characters'],
+      lowercase: true,
+      trim: true, 
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Please fill in a valid email address',
+      ], // Matches email against regex
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters'],
+      select: false, 
+    },
+    userType:{
+        type:String,
+        enum:['USER','ADMIN'],
+        default:'ADMIN'
+    }
+})
+const User=mongoose.model('User',UserSchema);
+module.exports=User;
