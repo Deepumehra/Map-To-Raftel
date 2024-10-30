53ec0b4a261fa27d61cec6fe53372d2b23747ac6
DHRUVRastogi-123 <57324344+DHRUVRastogi-123@users.noreply.github.com>
2024-10-30 06:49:36 +0000

    Trying to resolve some errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import LoginComponent from './Components/LoginComponent/Login'
 
 function App() {
   return (
     <>
-      App
+      <LoginComponent/>
     </>
   )
 }
--- frontend/src/Components/LoginComponent/Login.jsx
+++ frontend/src/Components/LoginComponent/Login.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
+import GoogleIcon from '@mui/icons-material/Google';
 
 const LoginComponent = () => {
 
     const handleGoogleSignUp = () => {
-        
+        console.log("Google Sign-Up Clicked"); // Placeholder action
     }
 
     return (
         <div className='login-button'>
-            <Button variant='outlined'>
-                
-                <span className='classes.buttonText'>Google SignIn</span>
+            <Button variant='outlined' onClick={handleGoogleSignUp}>
+                <GoogleIcon />
+                <span className='buttonText'>Google SignIn</span>
             </Button>
         </div>
-    )
+    );
 }
 
 export default LoginComponent;
