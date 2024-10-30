320b25ca6797abbebb60821478fa214d7f47a462
DHRUVRastogi-123 <57324344+DHRUVRastogi-123@users.noreply.github.com>
2024-10-30 06:17:30 +0000

    Designed the login Button

---
+++ frontend/src/Components/LoginComponent/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '@mui/material/Button'
+
+const LoginComponent = () => {
+
+    const handleGoogleSignUp = () => {
+        
+    }
+
+    return (
+        <div className='login-button'>
+            <Button variant='outlined'>
+                
+                <span className='classes.buttonText'>Google SignIn</span>
+            </Button>
+        </div>
+    )
+}
+
+export default LoginComponent;
