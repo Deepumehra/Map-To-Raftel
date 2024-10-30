4f91cea4b837c22fa40138700e6046093d2ccf67
DhruvRastogi-123 <powerfulrastogi@gmail.com>
2024-10-30 12:42:31 +0530

    Revert "Fake commit"
    This reverts commit 61811c00ee6a509ce492f9162aebb73ecac2efdb.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import LoginComponent from './Components/LoginComponent/Login';
-
+import './App.css'
+import LoginComponent from './Components/LoginComponent/Login'
 
 function App() {
-
-  const theme = createTheme();
-
   return (
-    <ThemeProvider theme={theme}>
-      <LoginComponent />
-    </ThemeProvider>
-  );
+    <>
+      <LoginComponent/>
+    </>
+  )
 }
 
-export default App;
+export default App
